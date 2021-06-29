30b96dfc0344712c64c541d5c984f5ea9005f746
Soliloquy <ibrahia@ucalgary.ca>
2021-06-29 22:51:44 +0000

    Adjust listings table for query

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -8,7 +8,7 @@ INSERT INTO users (name, email, password) VALUES
 ('Clown', 'clown@rednose.com', 'password'),
 ('Ajax', 'ajax@gooddog.com', 'password');
 
-INSERT INTO category_list (name) VALUES 
+INSERT INTO category_list (name) VALUES
   ('Iron Man'),
   ('Star Lord'),
   ('Spiderman'),
@@ -28,19 +28,19 @@ INSERT INTO listings (
   city,
   province,
   post_code,
-  buyer_id,
+  active,
   category_id
-  ) VALUES 
-  (1, 'Iron Man MK5', 'State of the art super suit, early edition','thumb photo','cover photo',50000,'USA','Ironman Lane','Malibu','California','12345',NULL,1),
-  (2, 'Iron Man MK50', 'State of the art super suit, battle authentic from Avengers Infinity War','thumb photo','cover photo',1000000000,'USA','Avengers Street','Avengers Campus','New York','12345',NULL,1),
-  (1, 'Infinity Gauntlet', 'Glorious weapon of mass destruction when containing all 5 stones, buy at your own risk. ','thumb photo','cover photo',50000,'Asgard','Thor Avenue','City of Asgard','Asgard','625891',NULL,6),
+  ) VALUES
+  (1, 'Iron Man MK5', 'State of the art super suit, early edition','thumb photo','cover photo',50000,'USA','Ironman Lane','Malibu','California','12345',FALSE,1),
+  (2, 'Iron Man MK50', 'State of the art super suit, battle authentic from Avengers Infinity War','thumb photo','cover photo',1000000000,'USA','Avengers Street','Avengers Campus','New York','12345',FALSE,1),
+  (1, 'Infinity Gauntlet', 'Glorious weapon of mass destruction when containing all 5 stones, buy at your own risk. ','thumb photo','cover photo',50000,'Asgard','Thor Avenue','City of Asgard','Asgard','625891',FALSE,6),
   (4, 'Batmobile', 'Matte black Lamborghimi used to fight crime at a fast pace','thumb photo','cover photo',1000000,'USA','Bat ave','Gotham','California','12345',NULL,5),
-  (3, 'Space Jet Boots', 'If you need to fly around, these are your boots of choice, get their fast and speedy.','thumb photo','cover photo',50000,'Zandar','space street','Zandar','Some planet','12345',NULL,2),
-  (2, 'High tensile spider silk', 'Reload your web shooters with 10km of the strongest spider silk to ensure you catch the villian.','thumb photo','cover photo',3000,'USA','Some back alley','Manhatten','New York','12345',NULL,3),
-  (3, 'Electric Stun grenades', 'Stun your enemy with 5000 volts of electricty with the most reliable stun grenades on the market.','thumb photo','cover photo',50,'Canada','Young Street','Toronto','Ontario','h5j9d1',NULL,4);
+  (3, 'Space Jet Boots', 'If you need to fly around, these are your boots of choice, get their fast and speedy.','thumb photo','cover photo',50000,'Zandar','space street','Zandar','Some planet','12345',FALSE,2),
+  (2, 'High tensile spider silk', 'Reload your web shooters with 10km of the strongest spider silk to ensure you catch the villian.','thumb photo','cover photo',3000,'USA','Some back alley','Manhatten','New York','12345',FALSE,3),
+  (3, 'Electric Stun grenades', 'Stun your enemy with 5000 volts of electricty with the most reliable stun grenades on the market.','thumb photo','cover photo',50,'Canada','Young Street','Toronto','Ontario','h5j9d1',FALSE,4);
 
-  INSERT INTO favorites (created_at, buyer_id, listing_id) 
-  VALUES 
+  INSERT INTO favorites (created_at, buyer_id, listing_id)
+  VALUES
   ('2021-06-10 17:00:00',5, 1),
   ('2021-06-11 17:00:00',6, 2),
   ('2021-06-12 17:00:00',5, 4),
@@ -50,7 +50,7 @@ INSERT INTO listings (
   ('2021-06-14 17:00:00',7, 2);
 
   INSERT INTO messages (to_id, from_id, time_sent, message, listing_id)
-  VALUES 
+  VALUES
   (2,6, '2021-06-26 15:00:000', 'I am interested in your MK50 suit', 2),
   (2,6, '2021-06-26 15:30:000', 'Hello? Did you get my message?', 2),
   (6,2, '2021-06-26 15:40:000', 'Yes, I am only willing to take 1 billion cash', 2),
